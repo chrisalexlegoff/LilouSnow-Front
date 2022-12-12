1a495c4065b7c3d781aa812ae4d98c841e159f75
unknown <contact@christophe-le-goff.com>
2022-12-12 18:05:24 +0100

    mep styles et architecture

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import type { AppProps } from "next/app"
+import { useEffect } from "react"
+import "../styles/globals.css"
+import { fixMobileSize } from "./../lib/helpers/mobile-size-fix"
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Mise à niveau taille pour mobile -- vh
+    fixMobileSize()
+  }, [])
+
   return <Component {...pageProps} />
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Head, Html, Main, NextScript } from "next/document"
+import React from "react"
+
+const _document = () => {
+  return (
+    <Html lang="fr">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default _document
