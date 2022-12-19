588ddb90906c7342dea63214c56b541be04c4be4
unknown <contact@christophe-le-goff.com>
2022-12-19 16:43:25 +0100

    fix api

--- lib/constants/api.ts
+++ lib/constants/api.ts
@@ -1,6 +1,6 @@
 // URLS BACKEND
 // NAV
-// const api_nav_main_desktop = "/navigation/render/main-navigation-desktop"
+const api_nav_main_desktop = "/navigation/render/main-navigation-desktop"
 const api_nav_main_mobile = "/navigation/render/main-navigation-mobile"
 
 // PAGES
@@ -8,6 +8,7 @@ const api_home_mobile = "/home-mobile"
 
 // export constantes ...
 export const API = {
+  NAV_MAIN_DESKTOP: api_nav_main_desktop,
   NAV_MAIN_MOBILE: api_nav_main_mobile,
   HOME_MOBILE: api_home_mobile,
 }
